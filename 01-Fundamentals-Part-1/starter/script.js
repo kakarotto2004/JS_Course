40b0b8075f6b925fe759dd0d29b49a1b0513ea5a
Jakub Pastucha <kuba.pastucha@gmail.com>
2023-10-02 22:26:29 +0200

    challenge #1 completed

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,31 @@
+// let js = "amazing";
+// console.log(40 + 2 + 120 * 2);
+
+// console.log("Jones");
+// console.log(23);
+
+// let firstName = "Jonas";
+// console.log(firstName);
+
+// let PI = 3.1415;
+
+// let myFirstJob = "Proframmer";
+// let mySecondJob = "Teacher";
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "23");
+
+
+// CHALLENGE #1
+let massMark = 78, heightMark = 1.69, massJohn = 92, heightJohn = 1.95;
+let BMIMark = massMark / (heightMark * heightMark), BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+let markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+
