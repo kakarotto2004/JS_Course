f8bc28431ee44883d4b9f90b5608dc485c47c060
kakarotto2211 <email@email.com>
2023-10-03 16:16:46 +0200

    CHALLENGE #2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -30,19 +30,56 @@
 // console.log(markHigherBMI);
 
 
-const firstName = "Jakub";
-const job = "IT technician";
-const birthYear = 2001;
-const currentYear = 2023;
+// const firstName = "Jakub";
+// const job = "IT technician";
+// const birthYear = 2001;
+// const currentYear = 2023;
 
-const aboutMe = "I'm " + firstName + ", a " + (currentYear - birthYear) + " year old " + job;
-console.log(aboutMe);
+// const aboutMe = "I'm " + firstName + ", a " + (currentYear - birthYear) + " year old " + job;
+// console.log(aboutMe);
 
-const aboutMeNew = `I'm ${firstName}, a ${currentYear - birthYear} year old ${job}`;
-console.log(aboutMeNew);
+// const aboutMeNew = `I'm ${firstName}, a ${currentYear - birthYear} year old ${job}`;
+// console.log(aboutMeNew);
 
-console.log('String with \n\
-multile lines\n\
-example');
+// console.log('String with \n\
+// multile lines\n\
+// example');
 
 
+
+// const age = 15;
+// const isOldEnaugh = age >= 18;
+
+// if (isOldEnaugh) {
+//     console.log("Sarah can start driving license");
+// } else {
+//     console.log("Sarah can't start driving license. She needs " + (18 - age) + " more years");
+// }
+
+// let century;
+// const birthYear = 2001;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+
+// console.log(century);
+
+
+
+// CHALLENGE #1
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIJohn > BMIMark) {
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
+} else {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+}
