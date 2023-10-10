652949e7b58a0648aba5106f9dc8d2c97652c1a3
kakarotto2211 <email@email.com>
2023-10-10 13:10:38 +0200

    CHALLENGE #3

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -69,7 +69,7 @@
 
 
 // CHALLENGE #1
-const massMark = 78;
+/*const massMark = 78;
 const heightMark = 1.69;
 const massJohn = 92;
 const heightJohn = 1.95;
@@ -82,4 +82,63 @@ if (BMIJohn > BMIMark) {
     console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`);
 } else {
     console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+}*/
+
+
+// Equality Operators: == vs. ===
+/*const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
+
+if (age == 18) console.log('You just became an adult :D (loose)');
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) { // 22 === 23 -> FALSE
+    console.log('Cool! 23 is an amzaing number!')
+} else if (favourite === 7) {
+    console.log('7 is also a cool number')
+} else if (favourite === 9) {
+    console.log('9 is also a cool number')
+} else {
+    console.log('Number is not 23 or 7 or 9')
+}
+
+if (favourite !== 23) console.log('Why not 23?');*/
+
+
+// LOGICAL OPERATORS
+/*const hasDrivingLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDrivingLicense && hasGoodVision);
+console.log(hasDrivingLicense || hasGoodVision);
+
+const shouldDrive = hasDrivingLicense && hasGoodVision;
+
+if (shouldDrive) {
+    console.log('Sarah is able to drive')
+} else {
+    console.log('Someone else should drive...')
+}
+
+const isTired = false;
+if (shouldDrive && !isTired) {
+    console.log('Sarah is able to drive')
+} else {
+    console.log('Someone else should drive...')
+}*/
+
+
+// CHALLENGE #3
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+
+if (scoreDolphins > scoreKoalas) {
+    console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins) {
+    console.log("Koalas win the trophy");
+} else {
+    console.log("Both win the trophy");
 }
