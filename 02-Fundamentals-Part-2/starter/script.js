18788018fb9736acdae7fd167d865f6e2bb877d6
kakarotto2211 <email@email.com>
2023-10-11 15:11:31 +0200

    arrow functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -10,6 +10,55 @@ if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
-// const private = 534; 
+// const private = 534;
 */
 
+
+// FUNCTIONS
+/*
+function logger() {
+    console.log("My name is Jonas");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+*/
+
+/*
+function calcAge1(birthYear) {
+    return 2023 - birthYear;
+}
+const age1 = calcAge1(2001);
+console.log(age1);
+
+const calcAge2 = function (birthYear) {
+    return 2023 - birthYear;
+}
+const age2 = calcAge2(2000);
+console.log(age2);
+
+const calcAge3 = birthYear => 2023 - birthYear;
+const age3 = calcAge3(1999);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2023 - birthYear;
+    const retirement = 65 - age;
+    return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1970, "Bob"));
+*/
