8a6c1fa8f2119aabf0210f68e86a364f128c0d8e
Jakub Pastucha <kuba.pastucha@gmail.com>
2023-10-19 14:22:28 +0200

    implement game logic

---
+++ 05-Guess-My-Number/starter/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/*console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highestScore = 0;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!';
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too hight!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent =
+                'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent =
+                'You lost the game!';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
