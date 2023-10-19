ca9302d16b5d4059b55d6a22d6505405c4f45ec7
kakarotto2004 <kuba.pastucha@gmail.com>
2023-10-19 19:32:11 +0200

    eliminate some repeted code

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -18,24 +18,46 @@ let score = 20;
 let highscore = 0;
 // document.querySelector('.number').textContent = secretNumber;
 
+const displayMessage = function (message) {
+    document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
 
     if (!guess) {
-        document.querySelector('.message').textContent = 'No number!';
+        // document.querySelector('.message').textContent = 'No number!';
+        displayMessage('No number!');
     } else if (guess === secretNumber) {
         document.body.style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '25rem';
 
         document.querySelector('.number').textContent = secretNumber;
-        document.querySelector('.message').textContent = 'Correct Number!';
+
+        // document.querySelector('.message').textContent = 'Correct Number!';
+        displayMessage('Correct Number!');
 
         if (score > highscore) {
             highscore = score;
             document.querySelector('.highscore').textContent = highscore;
         }
-    } else if (guess > secretNumber) {
+    } else if (guess !== secretNumber) {
+        // if guess higher then secretNumber diplays 'Too low' otherwise display 'Too low'
+        // document.querySelector('.message').textContent = guess > secretNumber ? 'Too hight!' : 'Too low!';
+        displayMessage(guess > secretNumber ? 'Too hight!' : 'Too low!');
+
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else {
+        // document.querySelector('.message').textContent = 'You lost the game!';
+        displayMessage('You lost the game!');
+
+        document.querySelector('.score').textContent = 0;
+    }
+
+    // DRY = dont repeat yourself
+    /*else if (guess > secretNumber) {
         if (score > 1) {
             document.querySelector('.message').textContent = 'Too hight!';
             score--;
@@ -55,7 +77,7 @@ document.querySelector('.check').addEventListener('click', function () {
                 'You lost the game!';
             document.querySelector('.score').textContent = 0;
         }
-    }
+    }*/
 });
 
 document.querySelector('.again').addEventListener('click', function () {
@@ -63,8 +85,11 @@ document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     document.querySelector('.number').textContent = '?';
     document.querySelector('.score').textContent = score;
+
     document.querySelector('.guess').value = '';
-    document.querySelector('.message').textContent = 'Start quessing...';
+    // document.querySelector('.message').textContent = 'Start quessing...';
+    displayMessage('Start guessing...');
+
     document.body.style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem';
 });
